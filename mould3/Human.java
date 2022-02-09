3aef1efac91e82bfa227932c5cdbe8b98dee9531
springvalley <89388846+springvalley@users.noreply.github.com>
2022-02-09 21:18:33 +0100

    Update Human.java

--- mould3/Human.java
+++ mould3/Human.java
@@ -6,6 +6,8 @@ public class Human
     private Head head;
     private Limb arm1;
     private Limb arm2;
+    private Limb leg1;
+    private Limb leg2;
     
     public Human (String name, int age){
         this.name = name;
@@ -13,11 +15,19 @@ public class Human
         this.head = new Head();
         this.arm1 = new Limb(true);
         this.arm2 = new Limb(true);
+        this.leg1 = new Limb(false);
+        this.leg1 = new Limb(false);
         sayName();
+        sayAge();
+        head.laugh();
     }
     
     public void sayName(){
         head.say("Hi my name is " + name);
+    }
+    
+    public void sayAge(){
+        head.say("Hi I am " + age + " Years old" );
     }
     
     public void setAge(int age){
