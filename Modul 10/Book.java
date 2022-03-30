dbc20804c6f55169c9cc2c67c7352300c8d3092d
springvalley <89388846+springvalley@users.noreply.github.com>
2022-03-30 19:24:02 +0200

    Create Book.java

---
+++ Modul 10/Book.java
@@ -0,0 +1,14 @@
+
+public class Book
+{
+    private String name;
+    public Book(String name)
+    {
+        this.name = name;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+}
