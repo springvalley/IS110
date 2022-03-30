c055e303e1b1fc6c39cef7d5a02f189e7627df17
springvalley <89388846+springvalley@users.noreply.github.com>
2022-03-30 19:24:48 +0200

    Create TextBook.java

---
+++ Modul 10/TextBook.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class TextBook here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TextBook extends Book
+{
+    
+    public TextBook(String name)
+    {
+        super(name);
+    }
+}
